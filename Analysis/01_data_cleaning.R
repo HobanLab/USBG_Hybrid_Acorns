f19b6bc5bbbbec07d6f19281b5df1daa5eb40cba
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2024-04-22 09:26:27 -0700

    Updating data cleaning analysis and moving files

--- Analysis/01_data_cleaning.R
+++ Analysis/01_data_cleaning.R
@@ -2,7 +2,6 @@
 #     Libraries     #
 #####################
 
-library(diveRsity)
 library(adegenet)
 library(poppr)
 
@@ -11,19 +10,15 @@ library(poppr)
 ###############################
 
 #set working directoy
-setwd("C:/Users/eschumacher/Desktop/UHA_Analysis")
-
-#convert arp file to genepop file 
-arp2gen("UHA_Final_Scores.arp")
+setwd("../Data_Files")
 
 #load in genepop file as a genind object
-UHA_fs_genind <- read.genepop("UHA_Final_Scores.gen", ncode = 3)
-
+UHA_genind <- read.genepop("Genotype_Files/2024_UHA_genepop.gen", ncode = 2)
 
 #reduce genind file for individuals with greater than 25% missing data 
-UHA_fs_genind_nomd <- missingno(UHA_fs_genind, type = "geno", 
+UHA_genind_nomd <- missingno(UHA_genind, type = "geno", 
                                 cutoff = 0.25, quiet = FALSE, freq = FALSE)
 
 #write out genind object as a genalex file
-genind2genalex(UHA_fs_genind_nomd,
-               "C:/Users/eschumacher/Desktop/UHA_Analysis/UHA_Final_Scores_nomd_genalex.csv")
+genind2genalex(UHA_genind_nomd,
+               "CSV_Files/UHA_Final_Scores_clean.csv")
