08ab49e9981f82d5ab81e620f6ff38aa0a6614ce
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2024-06-02 13:41:58 -0500

    updating file processing data frame to include scripts for generating parentage input data files that have all loci included or reduced numbers of loci for those loci with a high percentage of null alleles

--- Analysis/RScripts/01_data_cleaning_for_parentage.R
+++ Analysis/RScripts/01_data_cleaning_for_parentage.R
@@ -11,16 +11,13 @@ library(PopGenReport)
 ###########################
 
 #set working directoy
-setwd("../Data_Files")
+setwd("../..")
 
 #load in genepop file as a genind object
-UHA_genind <- read.genepop("Genotype_Files/2024_UHA_genepop.gen", ncode = 2)
-
-#load CERVUS data files
-UHA_CERVUS_prep <- read.csv("Parentage_Files/2024_04_22_CERVUS_Prep.csv")
+UHA_genind <- read.genepop("Data_Files/Genotype_Files/2024_UHA_genepop.gen", ncode = 2)
 
 #load score df
-UHA_scores_df <- read.csv("CSV_Files/UHA_score_df.csv")
+UHA_scores_df <- read.csv("Data_Files/CSV_Files/UHA_score_df.csv")
 
 ###############################
 #     Data Cleaning steps     #
@@ -32,26 +29,59 @@ UHA_genind_nomd <- missingno(UHA_genind, type = "geno",
 
 #write out genind object as a genalex file
 genind2genalex(UHA_genind_nomd,
-               "CSV_Files/UHA_Final_Scores_genalex_clean.csv")
+               "Data_Files/CSV_Files/UHA_Final_Scores_genalex_clean.csv")
 
 #limit by the cleaned individuals
 UHA_scores_clean_df <- UHA_scores_df[UHA_scores_df[,1] %in% 
                                        rownames(UHA_genind_nomd@tab),]
 
 #write out 
-write.csv(UHA_scores_clean_df, "CSV_Files/UHA_score_clean_df.csv")
+write.csv(UHA_scores_clean_df, "Data_Files/CSV_Files/UHA_score_clean_df.csv")
 
 ###########################
 #     Score Analysis      #
 ###########################
 
+####Null alleles
 #run null allele calculations over all genind objects
 UHA_null_all <- null.all(UHA_genind_nomd)
 
-#visualize 
+#store in a data frame 
 null_all_df <- signif(data.frame(UHA_null_all$null.allele.freq$summary2),3)*100
 
+#write out null allele data frame
+write.csv(null_all_df, "Results/Preliminary_Genotyping_Analysis/null_all_df.csv")
+
+####Linkage Disequilibrium
 #calculate linkage disequilbrium
 UHA_ld <- pair.ia(UHA_genind_nomd, sample = 1000)
+
 #convert to a data frame
 UHA_ld_df <- data.frame(round(UHA_ld, digits = 2))
+
+#write out ld df 
+write.csv(UHA_ld_df, "Results/Preliminary_Genotyping_Analysis/ld_df.csv")
+
+#########################################
+#     Prep Data Files for Parentage     #
+#########################################
+
+##Create data frames with and without loci with 
+#large number of null alleles
+
+#first identify which alleles have > 10% null alleles
+na_reorg_df <- as.data.frame(t(null_all_df))
+
+#list of loci with > 15% null alleles 
+na_loci <- rownames(na_reorg_df[which(na_reorg_df$`Observed frequency` > 15),])
+#remove underscores 
+na_loci <- gsub('_[^_]*$', '', na_loci)
+
+#generate input data frame for parentage - all loci
+write.csv(UHA_scores_clean_df, "Analysis/Parentage_Analysis/All_Loci/Input_Files/UHA_all_loci_par_df.csv",
+          row.names = FALSE)
+
+#generate a data frame removing alleles with high null allele %
+UHA_red_loci_df <- UHA_scores_clean_df %>%
+                    select(-contains(na_loci))
+
