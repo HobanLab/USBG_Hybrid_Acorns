9e23ee97f085c5d5d8bc39d42439ea7d73d251ee
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2024-06-02 13:09:50 -0500

    finishing script to write out distance models

--- Analysis/RScripts/04_dist_analysis.R
+++ Analysis/RScripts/04_dist_analysis.R
@@ -42,8 +42,9 @@ UHA_clean_par_df <- parentage_results %>%
 
 ###Create data frames with distances
 #Create a column of the crosses of every parent individual
-all_potential_combo <- crossing(UHA_clean_df$Tissue_ID, 
-                                UHA_clean_df$Tissue_ID)
+all_potential_combo <- crossing(UHA_par_df$Tissue_ID, 
+                                UHA_par_df$Tissue_ID)
+
 colnames(all_potential_combo) <- c("Parent_1", "Parent_2")
 
 #remove rows where the parents are the same individual (selfing)
@@ -72,8 +73,6 @@ for(d in 1:nrow(potential_combo_dedup)){
   potential_combo_dedup$Parent_2_species[d] <- Parent_2_Database_row$Species
   
 }
-
-
 
 #reorganize the combination data frame to fix species naming conventions and to
 #have a hybrid column 
@@ -172,23 +171,28 @@ pf_df <- relevant_potential_combos %>%
 #the greater the mean realized pollination dsitance for that mother
 
 ###create a data frame to save the adjusted R-squared and p-value
-rf_hybrid_prop <- matrix(nrow = 3, ncol = 2)
+rf_hybrid_prop_dist_df <- matrix(nrow = 5, ncol = 2)
 
 # #adjusted R-squared and 
 # hybrid_pop_df <- t(as.data.frame(c(summary(lm(formula = Prop_hybrids~Mean_real_dist, data=rf_df))[[9]],
 #                                    summary(lm(formula = Prop_hybrids~Mean_real_dist, data=rf_df))$coefficients[8])))
 
 #save p-values and r-squared results
-rf_hybrid_prop[1,1] <- summary(lm(formula = Prop_hybrids~Mean_real_dist, data=rf_df))$adj.r.squared
-rf_hybrid_prop[1,2] <- summary(lm(formula = Prop_hybrids~Mean_real_dist, data=rf_df))$coefficients[2,4]
-rf_hybrid_prop[2,1] <- summary(lm(formula = Prop_hybrids~Min_real_dist, data=rf_df))$adj.r.squared
-rf_hybrid_prop[2,2] <- summary(lm(formula = Prop_hybrids~Min_real_dist, data=rf_df))$coefficients[2,4]
-rf_hybrid_prop[3,1] <- summary(lm(formula = Prop_hybrids~Max_real_dist, data=rf_df))$adj.r.squared
-rf_hybrid_prop[3,2] <- summary(lm(formula = Prop_hybrids~Max_real_dist, data=rf_df))$coefficients[2,4]
+rf_hybrid_prop_dist_df[1,1] <- summary(lm(formula = Prop_hybrids~Mean_real_dist, data=rf_df))$adj.r.squared
+rf_hybrid_prop_dist_df[1,2] <- summary(lm(formula = Prop_hybrids~Mean_real_dist, data=rf_df))$coefficients[2,4]
+rf_hybrid_prop_dist_df[2,1] <- summary(lm(formula = Prop_hybrids~Min_real_dist, data=rf_df))$adj.r.squared
+rf_hybrid_prop_dist_df[2,2] <- summary(lm(formula = Prop_hybrids~Min_real_dist, data=rf_df))$coefficients[2,4]
+rf_hybrid_prop_dist_df[3,1] <- summary(lm(formula = Prop_hybrids~Max_real_dist, data=rf_df))$adj.r.squared
+rf_hybrid_prop_dist_df[3,2] <- summary(lm(formula = Prop_hybrids~Max_real_dist, data=rf_df))$coefficients[2,4]
+rf_hybrid_prop_dist_df[4,1] <- summary(lm(formula = Prop_hybrids~Mean_smallest_real_dists, data=rf_df))$adj.r.squared
+rf_hybrid_prop_dist_df[4,2] <- summary(lm(formula = Prop_hybrids~Mean_smallest_real_dists, data=rf_df))$coefficients[2,4]
+rf_hybrid_prop_dist_df[5,1] <- summary(lm(formula = Prop_hybrids~Mean_largest_real_dists, data=rf_df))$adj.r.squared
+rf_hybrid_prop_dist_df[5,2] <- summary(lm(formula = Prop_hybrids~Mean_largest_real_dists, data=rf_df))$coefficients[2,4]
 
 #add columns and row labels
-rownames(rf_hybrid_pop) <- c("Mean_Par_Dist", "Min_Par_Dist", "Max_Par_Dist")
-colnames(hybrid_pop_df) <- c("R2", "p-value")
+rownames(rf_hybrid_prop_dist_df) <- c("Mean_Par_Dist", "Min_Par_Dist", "Max_Par_Dist",
+                              "Mean_Smallest_Dist", "Mean_Largest_Dist")
+colnames(rf_hybrid_prop_dist_df) <- c("R2", "p-value")
 
 #write out plot comparing real fathers plot
 png("Results/Pairwise_Distance_Analysis/rf_dist_par_prop.png", width = 900,
@@ -201,6 +205,116 @@ rf_df %>%
   theme_classic()
 dev.off()
 
+# The linear model for the mean distance of the shortest 5 distances of real fathers
+#to a given mother shows no relationship between the distance of the closest 
+#5 realized fathers to a given mother and the proportion of offspring that were 
+#hybrids for that mother.
+rf_df %>%
+  ggplot() +
+  geom_point(aes(y = Prop_hybrids, x = Mean_smallest_real_dists)) +
+  geom_smooth(aes(y = Prop_hybrids, x = Mean_smallest_real_dists), 
+              method='lm', formula= y~x) +
+  xlab("Mean Distance Between Parents (m) - Shortest Distances") + 
+  ylab("Proportion of Hybrid Offspring") +
+  theme_classic()
+
+# The linear model for the mean distance of the largest 5 distances of real fathers
+#to a given mother 
+rf_df %>%
+  #filter(Mean_largest_real_dists < 500) %>% #to remove the largest value individual if you want
+  ggplot() +
+  geom_point(aes(y = Prop_hybrids, x = Mean_largest_real_dists)) +
+  geom_smooth(aes(y = Prop_hybrids, x = Mean_largest_real_dists), 
+              method='lm', formula= y~x) +
+  xlab("Mean Distance Between Parents (m) - Largest Distances") + 
+  ylab("Proportion of Hybrid Offspring") +
+  theme_classic()
+
+# note that when you remove the mom with the largest values (> 500), 
+#the significance disappears but the R2 is still pretty good
+summary(lm(formula = Prop_hybrids~Mean_largest_real_dists, 
+           data=filter(rf_df, Mean_largest_real_dists < 500)))
+
+write.csv(rf_hybrid_prop_dist_df, "Results/Pairwise_Distance_Analysis/rf_hybrid_prop_dist.csv")
+
+#####Potential fathers to a given mother df 
+###create a data frame to save the adjusted R-squared and p-value
+pf_hybrid_prop_dist <- matrix(nrow = 6, ncol = 2)
+
+# The linear model for the mean distance of a potential fathers to a given mother split by conspecific and heterospecific fathers. There is no relationship between the proportion of hybrid offspring and the mean distances of all potential het or con fathers.
+pf_hybrid_prop_dist[1,1] <- summary(lm(formula = Prop_hybrids~Mean_potential_dist, 
+                                       data=filter(pf_df, 
+                                                   Parental_species_match == "Conspecific")))$adj.r.squared
+pf_hybrid_prop_dist[1,2] <- summary(lm(formula = Prop_hybrids~Mean_potential_dist, 
+                                       data=filter(pf_df, 
+                                                   Parental_species_match == "Conspecific")))$coefficients[2,4]
+#mean distance to potential fathers, conspecific 
+pf_hybrid_prop_dist[2,1] <- summary(lm(formula = Prop_hybrids~Mean_potential_dist, data=filter(pf_df, Parental_species_match == "Heterospecific")))$adj.r.squared
+pf_hybrid_prop_dist[2,2] <- summary(lm(formula = Prop_hybrids~Mean_potential_dist, data=filter(pf_df, Parental_species_match == "Heterospecific")))$coefficients[2,4]
 
 
+#The linear model for the smallest distance of a potential father to a given 
+#mother split by conspecific and heterospecific fathers. 
+#There is no relationship between the proportion of hybrid offspring and the 
+#minimum distances of all potential het or con fathers.
+pf_hybrid_prop_dist[3,1] <- summary(lm(formula = Prop_hybrids~Min_potential_dist, data=filter(pf_df, Parental_species_match == "Conspecific")))$adj.r.squared
+pf_hybrid_prop_dist[3,2]<- summary(lm(formula = Prop_hybrids~Min_potential_dist, data=filter(pf_df, Parental_species_match == "Conspecific")))$coefficients[2,4]
 
+
+pf_hybrid_prop_dist[4,1] <- summary(lm(formula = Prop_hybrids~Min_potential_dist, data=filter(pf_df, Parental_species_match == "Heterospecific")))$adj.r.squared
+pf_hybrid_prop_dist[4,2]<- summary(lm(formula = Prop_hybrids~Min_potential_dist, data=filter(pf_df, Parental_species_match == "Heterospecific")))$coefficients[2,4]
+
+
+# The linear model for the mean distance of the shortest 5 distances of potential fathers to a given mother split by conspecific and heterospecific fathers. There is no relationship between the proportion of hybrid offspring and the 5 smallest distances of all potential het or con fathers.
+pf_hybrid_prop_dist[5,1] <- summary(lm(formula = Prop_hybrids~Mean_smallest_potential_dists, data=filter(pf_df, Parental_species_match == "Conspecific")))$adj.r.squared
+pf_hybrid_prop_dist[5,2] <- summary(lm(formula = Prop_hybrids~Mean_smallest_potential_dists, data=filter(pf_df, Parental_species_match == "Conspecific")))$coefficients[2,4]
+
+pf_hybrid_prop_dist[6,1] <- summary(lm(formula = Prop_hybrids~Mean_smallest_potential_dists, data=filter(pf_df, Parental_species_match == "Heterospecific")))$adj.r.squared
+pf_hybrid_prop_dist[6,2] <- summary(lm(formula = Prop_hybrids~Mean_smallest_potential_dists, data=filter(pf_df, Parental_species_match == "Heterospecific")))$coefficients[2,4]
+
+#add rownames and colnames
+rownames(pf_hybrid_prop_dist) <- c("Mean_Dist_Con", "Mean_Dist_Het", "Min_Dist_Con",
+                                   "Min_Dist_Het", "Max_Dist_Con", "Max_Dist_Het")
+colnames(pf_hybrid_prop_dist) <- c("R2", "p-value")
+
+write.csv(pf_hybrid_prop_dist, "Results/Pairwise_Distance_Analysis/pf_hybrid_prop_dist.csv")
+
+#########################################
+#     Resampling Distance Analysis      #
+#########################################
+
+# A for loop that, across each mom, randomly samples the pool of the 
+#possible conspecific dads (of which there are 20 out of the 280 adult 
+#individuals we have data on) 1000 times 
+#and records the distance between that dad and the given mom 
+
+
+set.seed(2024) #since we are using the sample function, set the seed for repeatability 
+
+#create the table that will hold the results of the for loop with 3 columns: Mom, Dad, and dist
+parents_dists_table_full <- tibble(Mom = character(), Dad = character(), dist = character())
+
+
+for(i in 1:length(unique(relevant_potential_combos$Parent_1))){
+  parents_dists_table_small <- tibble(Mom = character(), Dad = character(), dist = character()) #make a small version of the final table that will be overwritten with each new mom
+  mom <- unique(relevant_potential_combos$Parent_1)[i]  #get the ID of the given mom
+  mom_specific_combos <- relevant_potential_combos %>%
+    filter(Parent_1 == mom) %>%  #filter all possible combos to only those with the given mom as Parent_1
+    filter(Parental_species_match == "Conspecific")  #filter all possible combos with the given mom to only those that are the same species as the given mom
+  
+  possible_dads <- mom_specific_combos$Parent_2 #make a vector with a list of all of the possible dads by pulling the Parent_2 column of the mom_specific_combos df
+  
+  dads <- sample(possible_dads, size=1000, replace=T)  #randomly draw 1000 samples from all possible dads with replacement
+  
+  #a for loop that will loop through each of the 1000 sampled dads will make and add a row for the parents_dists_table_small 
+  for(x in 1:length(dads)){
+    dad <- dads[x] #get the ID of the given dad
+    parent_combo_row_for_table <- tibble(Mom = NA, Dad = NA, dist = NA) #creating table that will be a single row and will be overwritten during each iteration of the loop
+    parent_combo_row_for_table$Mom <- mom #add ID of the given mom to table
+    parent_combo_row_for_table$Dad <- dad #add ID of the given dad to table
+    parent_combo_row_for_table$dist <- filter(mom_specific_combos, Parent_1 == mom & Parent_2 == dad)$dist #add dist from mom to dad in table (by matching the mom and dad's IDs to the mom_specific_combos df)
+    
+    parents_dists_table_small <- rbind(parents_dists_table_small, parent_combo_row_for_table) #bind the row of the given dad to the  table of the given mom
+  }
+  parents_dists_table_full <- rbind(parents_dists_table_full, parents_dists_table_small)  #bind the table of the given given to the  table of all the moms
+}
