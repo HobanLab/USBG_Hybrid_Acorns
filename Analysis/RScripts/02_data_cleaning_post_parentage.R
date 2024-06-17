0916ff4bdffb0a57a6eee36b80794680632e82dd
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2024-06-17 16:46:11 -0500

    updating scripts with loops for removing low confidence results

--- Analysis/RScripts/02_data_cleaning_post_parentage.R
+++ Analysis/RScripts/02_data_cleaning_post_parentage.R
@@ -22,30 +22,86 @@ library(geosphere)
 #set working directory 
 setwd("../..")
 
-# parentage_results <- read_csv("UHA_11_14_23_parentage_analysis.csv")  # reading in parentage_results
-#spatial_parentage_results <- read_csv("UHA_11_14_23_parentage_analysis.csv")  # reading in parentage results again for future addition of spatial data
-#write in the parentage results summary 
-par_results <- read.csv("Analysis/Parentage_Analysis/Initial_Run/Output_Files/UHA_parentage_sumary.csv")
+#load parentage results - replacing:
+#par_results <- read.csv("Analysis/Parentage_Analysis/Initial_Run/Output_Files/UHA_parentage_sumary.csv")
 
-#replace periods with underscones 
-colnames(par_results) <-  gsub('\\.', '_', colnames(par_results))
+#load parentage results - all loci = al
+UHA_al_par <- read.csv("Analysis/Parentage_Analysis/All_Loci/Output_Files/UHA_all_loci_par_sum.csv",
+                       row.names = NULL)
 
-#parentage df 
-par_df <- read.csv("Data_Files/CSV_Files/UHA_score_clean_df.csv")
-#remove first column
-par_df <- par_df[,-1]
+#remove periods from UHA data frame
+colnames(UHA_al_par) <- gsub("\\.", "_", colnames(UHA_al_par))
 
-#rename offspring ID to tissue ID
-colnames(off_df)[1] <- "Offspring_ID"
+#load parentage results - reduced loci = rl
+UHA_rl_par <- read.csv("Analysis/Parentage_Analysis/Red_Loci/Output_Files/UHA_red_loci_par_sum.csv")
 
-#limit to offspring 
-off_df <- par_df[par_df$Parent_Offspring == "O",]
+#remove periods from colnames
+colnames(UHA_rl_par) <- gsub("\\.", "_", colnames(UHA_rl_par))
+
+##create a list of the parentage results data frames
+par_res_list <- list(UHA_al_par, UHA_rl_par)
+
+#create a list of scenarios 
+scen <- c("all_loci", "red_loci")
+
+#loop to reduce the parental summary data frames for low confidence father assignments
+for(sc in 1:length(scen)){
+  
+  temp_df <- par_res_list[[sc]]
+  
+  #first, join genotype files with parentage results 
+  lcf_df  <- temp_df[((temp_df$Pair_LOD_score > 0) & (temp_df$Trio_LOD_score > 0)),]
+  
+  #write out data frame 
+  write.csv(lcf_df, paste0("Data_Files/CSV_Files/UHA_", scen[[sc]], "_LCF_par_sum.csv"))
+  
+}
+ 
+#load in all parentage summary data frames 
+par_sum_list <- list.files(path = "Data_Files/CSV_Files/", pattern = "par_sum")
+
+#read CSVs 
+
+par_sum_df_list <- list()
+
+#Loop over 
+for(df in par_sum_list){
+  
+  par_sum_df_list[[df]] <- read.csv(paste0("Data_Files/CSV_Files/", df))
+  
+}
+
+##load score data frames
+#All loci
+al_score_df <- read.csv("Analysis/Parentage_Analysis/All_Loci/Input_Files/UHA_all_loci_genotype_df.csv")
+
+#Reduced loci
+rl_score_df <- read.csv("Analysis/Parentage_Analysis/Red_Loci/Input_Files/UHA_red_loci_genotype_df.csv")
+
+#combine into a list 
+score_df_list <- list(al_score_df, rl_score_df)
+
+
+###create offspring only data frames
+#list of offspring data frames
+
+off_df_list <- list()
+
+for(o in 1:length(score_df_list)){
+  
+  #limit data frame to just offspring for each scenario 
+  off_df_list[[o]] <- score_df_list[[o]][score_df_list[[o]]$Parent_Offspring == "O",]
+  
+  #rename "Tissue ID" to "Offspring_ID"
+  colnames(off_df_list[[o]])[1] <- "Offspring_ID"
+  
+}
 
 #load data file with geographic information 
 UHA_database <- read.csv("Data_Files/CSV_Files/ARCHIVED_USBG_Hybrid_Acorn_Tissue_Database.csv")
 
-#par_genotyoes <- read.csv("UHA_score_database_nomd_genotype - UHA_score_database_nomd_genotype.csv") #read in genotype file to use species information of maternal and paternal trees.
-#tissue_info <- read.csv("TCB_Tissue_Database.csv")
+#scenarios
+scen <- c("All_Loci", "Red_Loci")
 
 #################################################
 #     Data Cleaning Post Parentage Analysis     #
@@ -53,32 +109,42 @@ UHA_database <- read.csv("Data_Files/CSV_Files/ARCHIVED_USBG_Hybrid_Acorn_Tissue
 
 ###add parental species to the data frame 
 
-#joining the genotypes with the parentage results 
-#to create the full_parentage data set that will be have spatial data, 
-#hybrid status, and half sibling status.
-full_parentage <- left_join(off_df, par_results, by="Offspring_ID")
 
-#Adding in Species Information for Maternal and Paternal trees
-keep_col_ID <- c("Offspring_ID","Mother_ID", "Candidate_father_ID", "Species")
 
-# Narrowing the data again after the join of data sets
-full_parentage <- full_parentage[keep_col_ID] 
-full_parentage <- full_parentage %>%
-                    rename('Mother_Species' = 'Species')  
+for(sc in 1:length(scen)){
+  
+  ##reorg data frame 
+  #Adding in Species Information for Maternal and Paternal trees
+  keep_col_ID <- c("Offspring_ID","Mother_ID", "Candidate_father_ID", "Species")
+  
+  # Narrowing the data again after the join of data sets
+  par_df <- par_df[keep_col_ID] 
+  par_df <- par_df %>% rename('Mother_Species' = 'Species')  
+  
+  #add paternal species name
+  par_df <- left_join(par_df, UHA_database, 
+                              by=c('Candidate_father_ID' = 'Tissue_ID'))  
+  
+  #rename columns 
+  par_df <- par_df %>% rename('Candidate_Father_Species' = 'Species')
+  
+  #reduce by empty columns 
+  keep_col_ID2 <- c("Offspring_ID","Mother_ID","Candidate_father_ID", 
+                    "Mother_Species", "Species")
+  
+  #reduce data frame by populated columns
+  par_df <- par_df[keep_col_ID2]
+  
+  
+}
 
-#add paternal species name
-full_parentage <- left_join(full_parentage, par_df, 
-                             by=c('Candidate_father_ID' = 'Tissue_ID'))  
 
-#reduce by empty columns 
-keep_col_ID2 <- c("Offspring_ID","Mother_ID","Candidate_father_ID", 
-                  "Mother_Species", "Species")
 
-#reduce data frame by populated columns
-full_parentage <- full_parentage[keep_col_ID2]
-#rename columns 
-full_parentage <- full_parentage %>%
-                    rename('Candidate_Father_Species' = 'Species')
+
+
+
+
+
 
 ###add geographic information for both parents to the data frame 
 
