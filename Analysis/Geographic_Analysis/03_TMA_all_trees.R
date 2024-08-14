6500f502c05d35665e7a52dfdf5e88217248e572
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2024-08-14 16:05:06 -0500

    updating file pathways
    updating file pathways for the new folders that were created, additionally fixing character problems in the "TMA all trees" file

--- Analysis/Geographic_Analysis/03_TMA_all_trees.R
+++ Analysis/Geographic_Analysis/03_TMA_all_trees.R
@@ -13,10 +13,10 @@ library(sjmisc)
 #####################
 
 #set wd 
-setwd("C:/Users/eschumacher/Documents/GitHub/USBG_Hybrid_Acorn")
+setwd("../..")
 
 #load in data file with all accessioned trees for the TMA 
-TMA_all_trees <- read.csv("Data_Files/Geographic_Files/TMA_all_trees.csv")
+TMA_all_trees <- openxlsx::read.xlsx("Data_Files/Geographic_Files/TMA_all_trees.xlsx")
 
 #loop to ID which individuals are quercus records 
 for(i in 1:nrow(TMA_all_trees)){
@@ -25,12 +25,8 @@ for(i in 1:nrow(TMA_all_trees)){
                                          ignore.case = TRUE)
 }
 
-#spot check if the loop worked using this df 
-TMA_all_trees_red <- TMA_all_trees[,c(1:6,40)]
-
 #limit by quercus records 
 TMA_quercus_occ <- TMA_all_trees[TMA_all_trees$Quercus_tf == TRUE,]
 
 #write out csv
-write.csv(TMA_quercus_occ,"Data_Files/Geographic_Files/TMA_quercus_occ.csv",
-          row.names = FALSE)
+openxlsx::write.xlsx(TMA_quercus_occ,"Data_Files/Geographic_Files/TMA_quercus_occ.xlsx")
