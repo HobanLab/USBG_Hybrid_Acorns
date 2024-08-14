8aaf68f409f652e26a96107299a64d05def3bfbb
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2024-08-14 14:50:29 -0500

    updating file pathways
    within this document, updating the file pathways to the new project folders created

--- Analysis/Geographic_Analysis/02_garden_summary_dfs.R
+++ Analysis/Geographic_Analysis/02_garden_summary_dfs.R
@@ -6,19 +6,21 @@
 ###########################
 #     Load Data Files     #
 ###########################
-
 #list out garden data files 
-setwd("C:/Users/eschumacher/Documents/GitHub/USBG_Hybrid_Acorn/Analyses/Geographic_Results")
+setwd("../..")
+
 #list out garden occ files 
-garden_list_occ <- list.files(pattern = "_occ_df.csv$")
+garden_list_occ <- list.files(pattern = "_occ_df.csv$",
+                              path = "Results/Geographic_Analyses")
 
 #######################################
 #     Create Summary Data Frames      #
 #######################################
 
-for(garden in 1:length(garden_list_occ)){
+for(garden in seq_along(garden_list_occ)){
   
-  sp_df <- read.csv(garden_list_occ[[garden]])
+  sp_df <- read.csv(paste0("./Results/Geographic_Analyses/",
+                           garden_list_occ[[garden]]))
   
   species_list <- unique(sp_df$Species_Name)
   
@@ -30,7 +32,8 @@ for(garden in 1:length(garden_list_occ)){
     
     garden_species_df[sp,1] <- length(sp_df[sp_df$Species_Name == species_list[[sp]],][,1])
    
-    write.csv(garden_species_df, paste0(gsub("\\..*",'', garden_list_occ[[garden]]), "_species_df.csv"))
+    write.csv(garden_species_df, 
+              paste0("./Results/Geographic_Analyses/",gsub("\\..*",'', garden_list_occ[[garden]]), "_species_df.csv"))
     
   }
   
